fe3364749499a955b98dff0afd4e3a9511546a5d
Anastasiia Moskalenko <ubuntu@ip-172-31-5-20.us-east-2.compute.internal>
2020-07-11 18:50:38 +0000

    new vesrsion gem

--- lib/validate.rb
+++ lib/validate.rb
@@ -4,7 +4,9 @@ module Validate
   end
 
   def validate_attempt(user_string)
-    return false unless user_string.to_i.positive? || user_string.size < @length_code || user_string.size > @length_code
+    return false unless user_string.to_i.positive?
+    return false if user_string.size < @length_code
+    return false if user_string.size > @length_code
 
     arr_of_num = user_string.to_i.digits.map { |num| num if (1..@range_sectret_number).to_a.include? num }.compact
     arr_of_num.size >= @length_code ? (return true) : (return false)
