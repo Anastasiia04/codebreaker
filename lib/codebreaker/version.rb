3e572b5da94ea6cc1a6a9de4490f1c65c931ef17
Anastasiia Moskalenko <ubuntu@ip-172-31-5-20.us-east-2.compute.internal>
2020-07-29 19:40:14 +0000

    change storage

--- lib/codebreaker/version.rb
+++ lib/codebreaker/version.rb
@@ -1,3 +1,3 @@
 module Codebreaker
-  VERSION = '0.1.21'.freeze
+  VERSION = '0.1.22'.freeze
 end
--- lib/storage.rb
+++ lib/storage.rb
@@ -7,16 +7,13 @@ module Codebreaker
     PATH_TO_FILE = File.join(FOLDER_NAME, FILE_NAME)
 
     def create_folder
-      Dir.mkdir(FOLDER_NAME) && File.new(PATH_TO_FILE, 'w') until file_exist
+      dirname = File.dirname(PATH_TO_FILE)
+      FileUtils.mkdir_p(dirname) && File.new(FILE_NAME, 'w') unless File.directory?(dirname)
     end
 
     def save_to_file(data)
       create_folder
       File.open(PATH_TO_FILE, 'a') { |file| file.write(data.to_yaml) }
-    end
-
-    def file_exist
-      File.exist?(PATH_TO_FILE)
     end
 
     def load_from_file
