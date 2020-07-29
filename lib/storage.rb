36998b62417f0925006eb94049677c99fa001b5e
Anastasiia Moskalenko <ubuntu@ip-172-31-5-20.us-east-2.compute.internal>
2020-07-29 22:20:42 +0000

    chenge storage

--- lib/storage.rb
+++ lib/storage.rb
@@ -2,18 +2,19 @@ require 'fileutils'
 
 module Codebreaker
   module Storage
-    FOLDER_NAME = 'db'.freeze
-    FILE_NAME = 'statistics.yml'.freeze
-    PATH_TO_FILE = File.join(FOLDER_NAME, FILE_NAME)
+    PATH_TO_FILE = 'db/statistics.yml'.freeze
 
     def create_folder
-      dirname = File.dirname(PATH_TO_FILE)
-      return FileUtils.mkdir_p(dirname) && File.new(FILE_NAME, 'w') if File.directory?(dirname)
+      Dir.mkdir(File.dirname(PATH_TO_FILE)) && File.new(PATH_TO_FILE, 'w') until file_exist
     end
 
     def save_to_file(data)
       create_folder
       File.open(PATH_TO_FILE, 'a') { |file| file.write(data.to_yaml) }
+    end
+
+    def file_exist
+      File.exist?(PATH_TO_FILE)
     end
 
     def load_from_file
